dc4407f3c0d687dba1e91070cc008244daa9e8d6
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-10-31 23:04:55 +0100

    Fix bug.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const https = require('https');
 
 const app = express();
 
-const requestedLinks = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -15,6 +14,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
+  const requestedLinks = [];
   res.render("index.ejs", {
     errMsg: "",
     requestedLinks: []
@@ -63,7 +63,6 @@ app.post("/", function(req, res) {
           }
         }
 
-        console.log(requestedLinks);
 
         res.render("index.ejs", {
           errMsg:"",
